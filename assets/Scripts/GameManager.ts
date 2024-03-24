170f67d1909cee047ca352478579efaf40619205
9t8 <63334933+9t8@users.noreply.github.com>
2024-03-24 08:11:30 -0700

    manager code

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -1,14 +1,29 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Prefab } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('GameManager')
-export class GameManager extends Component {
-    start() {
-
-    }
-
-    update(deltaTime: number) {
-        
-    }
+// Track grid type, pit (NONE) or solid road (STONE)
+enum BlockType {
+  NONE,
+  STONE,
 }
-
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+  // The runway prefab
+  @property({ type: Prefab })
+  public cubePrfb: Prefab | null = null;
+
+  // Total road length
+  @property
+  public roadLength = 50;
+
+  private road: BlockType[] = [];
+
+  start() {
+    this.generateRoad();
+  }
+
+  generateRoad() {}
+
+  update(deltaTime: number) {}
+}
